e284acd7089451c71e4d885780e931a93aa502ad
Ozzy <ozzy@laundrapp.com>
2017-01-30 16:36:50 +0000

    Declassification of Laundrone project

---
+++ app/Drones/PrototypeDrone.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Laundrapp\Laundrone\Drones;
+
+class PrototypeDrone
+{
+	/**
+	 * Speed of drone in miles per hour.
+	 *
+	 * @var int
+	 */
+	private $speed = 60;
+
+
+
+	/**
+	 * Units of fuel that the drone has.
+	 *
+	 * @var int
+	 */
+	private $fuelUnits = 10;
+
+
+
+    /**
+     * Number of miles the drone can cover for
+     * every unit of fuel.
+     *
+     * @var int
+     */
+	private $milesPerUnit = 10;
+}
